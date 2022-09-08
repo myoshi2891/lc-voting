44d3e55c07e4cef26a6d8d429d68c19e19e7b651
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-09-08 23:50:18 +0900

    Voting for Idea

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -16,11 +16,11 @@
         </div>
         <div class="mt-8">
             @if ($hasVoted)
-                <button
+                <button wire:click.prevent="vote"
                     class="w-20 bg-blue text-white border boder-blue hover:bg-blue-hover font-bold text-xxs uppercase rounded-xl
             transition duration-150 ease-in px-4 py-3">Voted</button>
             @else
-                <button
+                <button wire:click.prevent="vote"
                     class="w-20 bg-gray-200 border boder-gray-200 hover:border-gray-400 font-bold text-xxs uppercase rounded-xl
             transition duration-150 ease-in px-4 py-3">Vote</button>
             @endif
@@ -77,11 +77,11 @@
                         <div class="text-xss font-semibold leading-none text-gray-400">Votes</div>
                     </div>
                     @if ($hasVoted)
-                        <button
+                        <button wire:click.prevent="vote"
                             class="w-20 bg-blue text-white border border-blue font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover
                         transition duration-150 ease-in px-4 py-3 -mx-5">Voted</button>
                     @else
-                        <button
+                        <button wire:click.prevent="vote"
                             class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">Vote</button>
                     @endif
                 </div>
--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -58,10 +58,10 @@
                             <div class="text-xss font-semibold leading-none text-gray-400">Votes</div>
                         </div>
                         @if ($hasVoted)
-                            <button
+                            <button wire:click.prevent="vote"
                                 class="w-20 bg-blue text-white border border-blue font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5">Voted</button>
                         @else
-                            <button
+                            <button wire:click.prevent="vote"
                                 class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">Vote</button>
                         @endif
                     </div>
@@ -206,14 +206,14 @@
                 <div class="text-gray-400 text-xs leading-none">Votes</div>
             </div>
             @if ($hasVoted)
-                <button type="button"
+                <button type="button" wire:click.prevent="vote"
                     class="h-11 w-32 text-xs bg-blue text-white
             font-semibold uppercase rounded-xl border border-blue hover:bg-blue-hover
             transition duration-150 ease-in px-6 py-3">
                     <span>Voted</span>
                 </button>
             @else
-                <button type="button"
+                <button type="button" wire:click.prevent="vote"
                     class="h-11 w-32 text-xs bg-gray-200
             font-semibold uppercase rounded-xl border border-gray-200 hover:border-gray-400
             transition duration-150 ease-in px-6 py-3">

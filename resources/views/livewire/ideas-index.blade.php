588efb3abae1bb8be4da195756cb58594c2fbde7
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-09-10 15:16:09 +0900

    Status Filters Count

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -38,6 +38,7 @@
         @endforeach
     </div> <!-- end ideas-container -->
     <div class="my-8">
-        {{ $ideas->links() }}
+        {{-- {{ $ideas->links() }} --}}
+        {{ $ideas->appends(request()->query())->links() }}
     </div>
 </div>

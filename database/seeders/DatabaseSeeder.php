93c5782eb62f8eea37ff867da69e68d91dbc0c87
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-09-13 08:32:02 +0900

    Restart the project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use App\Models\Idea;
+use App\Models\Status;
 use App\Models\User;
 use App\Models\Vote;
-use App\Models\Status;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,8 +19,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory()->create([
-            'name' => 'Ann',
-            'email' => 'ann@example.com',
+            'name' => 'Andre',
+            'email' => 'andre_madarang@hotmail.com',
         ]);
 
         User::factory(19)->create();
@@ -37,14 +36,13 @@ class DatabaseSeeder extends Seeder
         Status::factory()->create(['name' => 'Implemented', 'classes' => 'bg-green text-white']);
         Status::factory()->create(['name' => 'Closed', 'classes' => 'bg-red text-white']);
 
-        // \App\Models\User::factory(10)->create();
         Idea::factory(100)->create();
 
-        $idea->isVotedByUser($user);
 
+        // Generate unique votes. Ensure idea_id and user_id are unique for each row
         foreach (range(1, 20) as $user_id) {
             foreach (range(1, 100) as $idea_id) {
-                if ($idea_id % 2 == 0) {
+                if ($idea_id % 2 === 0) {
                     Vote::factory()->create([
                         'user_id' => $user_id,
                         'idea_id' => $idea_id,
